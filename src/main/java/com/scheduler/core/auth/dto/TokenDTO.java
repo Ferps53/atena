8d3156a8af93ad842ee00eb6ce2d50de9b531689
ferps <felipebrostolinribeiro@gmail.com>
2025-08-10 18:33:27 -0300

    feat: add refresh_expires_in TokenDTO

--- src/main/java/com/scheduler/core/auth/dto/TokenDTO.java
+++ src/main/java/com/scheduler/core/auth/dto/TokenDTO.java
@@ -5,13 +5,16 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public record TokenDTO(
-        @JsonProperty("access_token") String accessToken,
-        @JsonProperty("refresh_token") String refreshToken,
-        @JsonProperty("expires_in") int expiresIn,
-        @JsonProperty("type") String tokenType
-) {
+    @JsonProperty("access_token") String accessToken,
+    @JsonProperty("refresh_token") String refreshToken,
+    @JsonProperty("expires_in") int expiresIn,
+    @JsonProperty("refresh_expires_in") int refreshExpiresIn,
+    @JsonProperty("type") String tokenType) {
 
-    public TokenDTO (String accessToken, String refreshToken) {
-      this (accessToken, refreshToken, 3600, "bearer");
-    }
+  static final int HOUR_IN_SECONDS = 60 * 60;
+  static final int FOUR_HOURS_IN_SECONDS = HOUR_IN_SECONDS * 4;
+
+  public TokenDTO(String accessToken, String refreshToken) {
+    this(accessToken, refreshToken, HOUR_IN_SECONDS, FOUR_HOURS_IN_SECONDS, "bearer");
+  }
 }
