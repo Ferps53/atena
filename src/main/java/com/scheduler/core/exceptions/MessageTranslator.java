0cfe55867ce92ff371c48f43c55677d7f0e91113
Felipe Brostolin Ribeiro <felipebrostolinribeiro@gmail.com>
2025-08-21 14:57:09 -0300

    style: fix code style

--- src/main/java/com/scheduler/core/exceptions/MessageTranslator.java
+++ src/main/java/com/scheduler/core/exceptions/MessageTranslator.java
@@ -1,23 +1,19 @@
 package com.scheduler.core.exceptions;
 
 import jakarta.enterprise.context.ApplicationScoped;
-
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 @ApplicationScoped
 public final class MessageTranslator {
 
-  private MessageTranslator() {
-  }
+  private MessageTranslator() {}
 
   public static String translate(String messageCode) {
 
     try {
       return ResourceBundle.getBundle(
-          "messages",
-          Locale.of("pt", "BR"),
-          Thread.currentThread().getContextClassLoader())
+              "messages", Locale.of("pt", "BR"), Thread.currentThread().getContextClassLoader())
           .getString(messageCode);
     } catch (Exception e) {
       return messageCode;
